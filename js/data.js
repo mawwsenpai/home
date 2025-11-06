1030f6a77a94039c2a8eff5ed94effe075cb6010
mawwsenpai <mawwmc332@gmail.com>
2025-11-06 20:34:22 +0700

    update

--- js/data.js
+++ js/data.js
@@ -9,7 +9,7 @@ const menuDataNested = {
       harga: "Rp 35.000",
       stok: "tersedia",
       deskripsi: "Deskripsi untuk Tongseng + Nasi akan segera hadir. Silakan tanyakan pada kami untuk info lebih lanjut.",
-      foto: ["img/menu/tongseng.png"], 
+      foto: ["img/menu/tongseng.jpg", "img/menu/tongsen2.jpg"], 
       wa_message: "Halo, saya mau pesan Tongseng + Nasi."
     },
     {
